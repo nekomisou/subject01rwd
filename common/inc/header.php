52eb0a5d6918d4ac14ec9bdb017c79121d7445cd
takasasaki <komm_susser_tod_taka@hotmail.co.jp>
2018-07-13 13:46:06 +0900

    new file:   .gitignore

---
+++ common/inc/header.php
@@ -0,0 +1,19 @@
+<? php
+    <header>
+        <h1>
+        <picture>
+            <source
+                media="(max-width: 767px)"
+                srcset="/common/images/kv.png 1x, /common/images/kv@2.png 2x"
+            >
+
+           <source
+                media="(min-width: 768px)"
+                srcset="/common/images/kv-big.png 1x, /common/images/kv-big@2.png 2x"
+           >
+
+           <img src="/common/images/kv.png" alt="春の海外旅行スペシャルトップ画像">
+        </picture>
+
+        </h1>
+    </header>
